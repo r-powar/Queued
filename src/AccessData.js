4b9c766a327377dcbf28fbfb5c1000b95f3f723f
Raj Powar <raaj.powar@gmail.com>
2017-06-04 17:11:39 -0700

    compiled server

--- src/AccessData.js
+++ src/AccessData.js
@@ -1,25 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * Created by rpowar on 4/30/17.
- */
-var Mongoose = require("mongoose");
-var AccessData = (function () {
-    function AccessData() {
+const Mongoose = require("mongoose");
+class AccessData {
+    constructor() {
         AccessData.connect();
     }
-    AccessData.connect = function () {
+    static connect() {
         if (this.mongooseInstance)
             return this.mongooseInstance;
         this.mongooseConnection = Mongoose.connection;
-        this.mongooseConnection.on("open", function () {
+        this.mongooseConnection.on("open", () => {
             console.log("Connected to mongodb.");
         });
         this.mongooseInstance = Mongoose.connect(this.DB_CONNECTION_STRING);
         return this.mongooseInstance;
-    };
-    return AccessData;
-}());
+    }
+}
 AccessData.DB_CONNECTION_STRING = 'mongodb://dbadmin:queued@ds064799.mlab.com:64799/queued';
 AccessData.connect();
 exports.default = AccessData;
+//# sourceMappingURL=AccessData.js.map
